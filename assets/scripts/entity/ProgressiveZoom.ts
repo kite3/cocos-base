a3e051c54c83df270711ab9066b8433f461b33b7
李金城 <lijincheng@37.com>
2025-08-29 14:54:07 +0800

    update: 完善渐进式出现脚本

--- assets/scripts/entity/ProgressiveZoom.ts
+++ assets/scripts/entity/ProgressiveZoom.ts
@@ -1,28 +1,42 @@
-import { _decorator, CCFloat, Component, Node } from "cc";
-import { progressiveZoom } from "../utils/common";
+import { _decorator, CCFloat, Component, Node } from 'cc';
+import { disableLayout, hideNodes, progressiveZoom } from '../utils/common';
 const { ccclass, property } = _decorator;
 
-@ccclass("ProgressiveZoom")
+@ccclass('ProgressiveZoom')
 export class ProgressiveZoom extends Component {
   @property({
     type: [Node],
-    tooltip: "需要递进出现的节点按顺序填入",
+    tooltip: '需要递进出现的节点按顺序填入'
   })
   nodeList: Node[] = [];
 
   @property({
     type: CCFloat,
-    tooltip: "间隔时间",
+    tooltip: '间隔时间'
   })
-  intervalDelay: number = 0.2;
+  intervalDelay: number = 0.15;
 
   @property({
     type: CCFloat,
-    tooltip: "动画时间",
+    tooltip: '动画时间'
   })
   animationDuration: number = 0.3;
 
+  @property({
+    type: CCFloat,
+    tooltip: '延迟开始时间'
+  })
+  delayTime: number = 0.2;
+
   start() {
-    progressiveZoom(this.nodeList, this.intervalDelay, this.animationDuration);
+    disableLayout(this.node);
+    hideNodes(this.nodeList);
+    this.scheduleOnce(() => {
+      progressiveZoom(
+        this.nodeList,
+        this.intervalDelay,
+        this.animationDuration
+      );
+    }, this.delayTime);
   }
 }
